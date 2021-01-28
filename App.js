793701517fc1da1b20880520bd312d5a365235f7
4BR40 <ronaldo.abrao@gmail.com>
2021-01-28 19:52:34 -0300

    Async Font loading

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator } from "react-native";
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import { enableScreens } from 'react-native-screens';
@@ -8,12 +8,12 @@ import { enableScreens } from 'react-native-screens';
 import MealsNavigator from './navigation/MealsNavigator';
 enableScreens();
 
-const fetchFonts = () => {
-  Font.loadAsync({
+async function fetchFonts() {
+  await Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
   });
-};
+}
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
