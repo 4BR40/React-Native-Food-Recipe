589abd59234a9f5cee6c0d7cec562a806a2e5033
4BR40 <ronaldo.abrao@gmail.com>
2021-02-03 02:50:58 -0300

    favorites done

--- srcreens/FavoritesScreen.js
+++ srcreens/FavoritesScreen.js
@@ -1,12 +1,10 @@
 import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
 
-
 import HeaderButton from '../components/HeaderButton';
 import MealList from '../components/MealList';
-import mealsReducer from '../store/reducers/meals';
-
 
 const FavoritesScreen = (props) => {
   React.useLayoutEffect(() => {
@@ -25,9 +23,23 @@ const FavoritesScreen = (props) => {
     });
   });
 
-  const favMeals = useSelector(state => state.meals.favoriteMeals);
+  const favMeals = useSelector((state) => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <Text>No favorite meals yet!</Text>
+      </View>
+    );
+  }
 
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
-
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
 export default FavoritesScreen;
