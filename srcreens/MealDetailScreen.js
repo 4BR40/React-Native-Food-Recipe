0140839f2310d3a1797d4166ae2f8b6728d322d1
4BR40 <ronaldo.abrao@gmail.com>
2021-02-03 02:19:35 -0300

    fav toggle star

--- srcreens/MealDetailScreen.js
+++ srcreens/MealDetailScreen.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import {
-  ScrollView,
-  View,
-  Image,
-  Text,
-  StyleSheet
-} from 'react-native';
+import { ScrollView, View, Image, Text, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import HeaderButton from '../components/HeaderButton';
 import DefaultText from '../components/DefaultText';
-import { startDetecting } from 'react-native/Libraries/Utilities/PixelRatio';
+import { toggleFavorite } from '../store/actions/meals';
 
-const ListItem = props => {
+const ListItem = (props) => {
   return (
     <View style={styles.listItem}>
       <DefaultText>{props.children}</DefaultText>
@@ -24,8 +18,13 @@ const ListItem = props => {
 const MealDetailScreen = (props) => {
   const mealId = props.route.params.mealId;
 
-  const availableMeals = useSelector(state => state.meals.meals);  
+  const availableMeals = useSelector((state) => state.meals.meals);
   const selectedMeal = availableMeals.find((meal) => meal.id === mealId);
+  const isFav = useSelector((state) =>
+    state.meals.favoriteMeals.some((meal) => meal.id === mealId)
+  );
+
+  const dispatch = useDispatch();
 
   React.useLayoutEffect(() => {
     props.navigation.setOptions({
@@ -34,9 +33,9 @@ const MealDetailScreen = (props) => {
         <HeaderButtons HeaderButtonComponent={HeaderButton}>
           <Item
             title="Favorite"
-            iconName="ios-star"
+            iconName={isFav ? 'ios-star' : 'ios-star-outline'}
             onPress={() => {
-              console.log('Mark');
+              dispatch(toggleFavorite(mealId));
             }}
           ></Item>
         </HeaderButtons>
@@ -45,7 +44,7 @@ const MealDetailScreen = (props) => {
   });
 
   return (
-<ScrollView>
+    <ScrollView>
       <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
       <View style={styles.details}>
         <DefaultText>{selectedMeal.duration}m</DefaultText>
@@ -53,11 +52,11 @@ const MealDetailScreen = (props) => {
         <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
       </View>
       <Text style={styles.title}>Ingredients</Text>
-      {selectedMeal.ingredients.map(ingredient => (
+      {selectedMeal.ingredients.map((ingredient) => (
         <ListItem key={ingredient}>{ingredient}</ListItem>
       ))}
       <Text style={styles.title}>Steps</Text>
-      {selectedMeal.steps.map(step => (
+      {selectedMeal.steps.map((step) => (
         <ListItem key={step}>{step}</ListItem>
       ))}
     </ScrollView>
@@ -67,25 +66,25 @@ const MealDetailScreen = (props) => {
 const styles = StyleSheet.create({
   image: {
     width: '100%',
-    height: 200
+    height: 200,
   },
   details: {
     flexDirection: 'row',
     padding: 15,
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
   },
   title: {
     fontFamily: 'open-sans-bold',
     fontSize: 22,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   listItem: {
     marginVertical: 10,
     marginHorizontal: 20,
     borderColor: '#ccc',
     borderWidth: 1,
-    padding: 10
-  }
+    padding: 10,
+  },
 });
 
 export default MealDetailScreen;
