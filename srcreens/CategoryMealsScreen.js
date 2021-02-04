bf2e1d0e638afad4745044b72641b9cabb6c1171
4BR40 <ronaldo.abrao@gmail.com>
2021-02-03 23:22:33 -0300

    filters done

--- srcreens/CategoryMealsScreen.js
+++ srcreens/CategoryMealsScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { View, StyleSheet } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
 import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
 
 const CategoryMealScreen = (props) => {
   const catId = props.route.params.categoryId;
@@ -21,7 +23,22 @@ const CategoryMealScreen = (props) => {
     (meal) => meal.categoryIds.indexOf(catId) >= 0
   );
 
+  if (displayedMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No meals found!</DefaultText>
+      </View>
+    );
+  }
+
   return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
 
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 export default CategoryMealScreen;
